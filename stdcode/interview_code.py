b1152ec92ebf0143c4afb99844d84f7853184401
Kodjo Adanledji <kad@maclt99.home>
2021-03-03 23:02:00 +0100

    new workflow

--- stdcode/interview_code.py
+++ stdcode/interview_code.py
@@ -320,8 +320,7 @@ def main(aoifilepath, years):
     a dict of rainy days monthly means over the years period given.
     '''
     # Selection of year(s) of interest
-    availableyears = [2020, 2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012, 2011, 2010, 2009, 2008, 2007, 2006, 2005, 2004, 2003, 2002,
-                      2001, 2000, 1999, 1998, 1997, 1996, 1995, 1994, 1993, 1992, 1991, 1990, 1989, 1988, 1987, 1986, 1985, 1984, 1983, 1982, 1981]
+    availableyears = [*range(1981, 2021, 1)]
 
     BASE_URL = 'https://data.chc.ucsb.edu/products/CHIRPS-2.0/global_daily/tifs/p25/'
     # check if selected years is in the available years
